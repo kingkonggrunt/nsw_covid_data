7b13fe1d0f322ce68af776ca9a8e364f6836e5cc
kingkonggrunt <little-kk@hotmail.com>
2022-02-25 18:11:12 +1100

    feat: added 404 error codes and improved lint

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, status, HTTPException
 from src import data
 from src import processing
 
@@ -17,10 +17,12 @@ class NGINXConfig():
 @app.get(NGINXConfig.uri + "/")
 def get_routes():
     """
-    Return the routes available for this API. As of writing this docstring (20-02-2022) FastAPI's --root-path parameter doesn't work
-    so FastAPI's autodocumentation doesn't work if the API is deployed on a URI proxy. Setting the root path to reveal available routes is a comprimise. 
+    Return the routes available for this API. As of writing this docstring
+    (20-02-2022) FastAPI's --root-path parameter doesn't work so FastAPI's
+    autodocumentation doesn't work if the API is deployed on a URI proxy.
+    Setting the root path to reveal available routes is a comprimise.
     """
-    
+
     return {
         "Welcome": "Routes are listed below",
         "Routes": {
@@ -60,20 +62,22 @@ def get_routes():
 @app.get(NGINXConfig.uri + "/age/total")
 def age_total():
     """Return the total covid cases for each age group"""
-    out = processing.AgeGroup()
-    return out.age_group_totals()
+    info = processing.AgeGroup()
+    out = info.age_group_totals()
+    return out
 
 @app.get(NGINXConfig.uri + "/age/{group}")
 def age_group_overtime(group: str, response: Response):
     """Return the daily cases data for an age group"""
-    out = processing.AgeGroup()
+    info = processing.AgeGroup()
+    out = info.age_group_overtime(group)
 
-    if not out.data:
-        response.status_code = 404
-        return "Invalid Age Group"
+    if not out:
+        raise HTTPException(status.HTTP_404_NOT_FOUND,
+                            detail=f"Age Group '{group}' not found")
 
     response.status_code = 200
-    return out.age_group_overtime(group)
+    return out
 
 
 # ===== Postcode Routes
@@ -81,48 +85,42 @@ def age_group_overtime(group: str, response: Response):
 @app.get(NGINXConfig.uri + "/location/postcode/{postcode}")
 def return_postcode_overtime(postcode, response: Response):
     """Return the daily cases data for a postcode"""
-    out = processing.CaseLocation()
+    info = processing.CaseLocation()
+    out = info.postcode(postcode)
 
-    
-    # if not out.postcode(postcode):
-    #     response.status_code = 404
-    #     return "Invalid Postcode"
-    # else:
-    #     response.status_code = 200
-    return out.postcode(postcode)
-        
+    if not out:
+        raise HTTPException(status.HTTP_404_NOT_FOUND,
+                            detail=f"Postcode '{postcode}' not found")
+
+    response.status_code = 200
+    return out
+
 
 @app.get(NGINXConfig.uri + "/location/lga/{lga}")
 def return_lga_overtime(lga, response: Response):
     """Return the daily cases data for an lga"""
-    out = processing.CaseLocation()
-    return out.lga(lga)
+    info = processing.CaseLocation()
+    out = info.lga(lga)
 
-    # if not out.data:
-    #     response.status_code = 404
-    #     return "Invalid LGA Code or Name"
+    if not out:
+        raise HTTPException(status.HTTP_404_NOT_FOUND,
+                            detail=f"LGA '{lga}' not found")
 
-    # response.status_code = 200
-    # return out.data
+    response.status_code = 200
+    return out
 
 @app.get(NGINXConfig.uri + "/location/lhd/{lhd}")
 def return_lhd_overtime(lhd, response: Response):
     """Return the daily cases data for an lhd"""
-    out = processing.CaseLocation()
-    return out.lhd(lhd)
+    info = processing.CaseLocation()
+    out = info.lhd(lhd)
 
-    # if not out.data:
-        # response.status_code = 404
-        # return "Invalid LHD Code or Name"
-# 
-    # response.status_code = 200
-    # return out.data
+    if not out:
+        raise HTTPException(status.HTTP_404_NOT_FOUND,
+                            detail=f"LHD '{lhd}' not found")
 
-@app.get(NGINXConfig.uri + "/location/{type}")
-def return_locations(type: str):
-    """Return the list of available postcodes, lgas, and lhd the user can request data for"""
-    out = processing.CaseLocation()
-    return out.list_locations(type)
+    response.status_code = 200
+    return out
 
 
 # ===== Testing Routes
@@ -130,44 +128,52 @@ def return_locations(type: str):
 @app.get(NGINXConfig.uri + "/tests/postcode/{postcode}")
 def test_postcode(postcode: str, response: Response):
     """Return testing data for a postcode"""
-    out = processing.Tests()
-    return out.postcode(postcode)
+    info = processing.Tests()
+    out = info.postcode(postcode)
 
-    # if not out.data:
-    #     response.status_code = 404
-    #     return "Invalid Postcode"
+    if not out:
+        raise HTTPException(status.HTTP_404_NOT_FOUND,
+                            detail=f"Postcode '{postcode}' not found")
 
-    # response.status_code = 200
-    # return out.data
+    response.status_code = 200
+    return out
 
 @app.get(NGINXConfig.uri + "/tests/lga/{lga}")
 def test_lga(lga: str, response: Response):
     """Return testing data for an lga"""
-    out = processing.Tests()
-    return out.lga(lga)
+    info = processing.Tests()
+    out = info.lga(lga)
 
-    # if not out.data:
-    #     response.status_code = 404
-    #     return "Invalid LGA Code or Name"
+    if not out:
+        raise HTTPException(status.HTTP_404_NOT_FOUND,
+                            detail=f"lga '{lga}' not found")
 
-    # response.status_code = 200
-    # return out.data
+    response.status_code = 200
+    return out
 
 @app.get(NGINXConfig.uri + "/tests/lhd/{lhd}")
 def tests_lhd(lhd: str, response: Response):
     """Return testing data for an lhd"""
-    out = processing.Tests()
-    return out.lhd(lhd)
+    info = processing.Tests()
+    out = info.lhd(lhd)
 
-    # if not out.data:
-    #     response.status_code = 404
-    #     return "Invalid LHD Code or Name"
+    if not out:
+        raise HTTPException(status.HTTP_404_NOT_FOUND,
+                            detail=f"lhd '{lhd}' not found")
 
-    # response.status_code = 200
-    # return out.data
-    
-@app.get(NGINXConfig.uri + "/tests/{type}")
-def return_locations(type: str):
+    response.status_code = 200
+    return out
+
+@app.get(NGINXConfig.uri + "/tests/{location_type}")
+@app.get(NGINXConfig.uri + "/location/{location_type}")
+def return_locations(location_type: str, response: Response):
     """Return the list of available postcodes, lgas, and lhd the user can request data for"""
-    out = processing.CaseLocation()
-    return out.list_locations(type)
+    info = processing.CaseLocation()
+    out =  info.list_locations(location_type)
+
+    if not out:
+        raise HTTPException(status.HTTP_404_NOT_FOUND,
+                            detail="Invalid location type. Use one of 'lga, postcode, or lhd'")
+
+    response.status_code = 200
+    return out
