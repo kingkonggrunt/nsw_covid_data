1007a4dd17c288e7cd2ccc330954c41ea91a4da2
kingkonggrunt <little-kk@hotmail.com>
2022-02-26 04:19:44 +1100

    feat: added caching to test_data routes

--- src/routers/test_data.py
+++ src/routers/test_data.py
@@ -1,6 +1,11 @@
+import json
 from fastapi import APIRouter, Response, status, HTTPException
+import redis
 from ..proxy import NGINXConfig
 from .. import processing
+from src.cache.redis import RedisDictionary
+
+r = RedisDictionary(redis.Redis, db=5)
 
 router = APIRouter(
     prefix=f"{NGINXConfig.uri}/tests"
@@ -9,6 +14,12 @@ router = APIRouter(
 @router.get("/postcode/{postcode}")
 def test_postcode(postcode: str, response: Response):
     """Return testing data for a postcode"""
+    response.status_code = 200
+    cache_key = f"tests_{postcode}"
+
+    if cache_key in r:
+        return json.loads(r[cache_key])
+
     info = processing.Tests()
     out = info.postcode(postcode)
 
@@ -16,12 +27,18 @@ def test_postcode(postcode: str, response: Response):
         raise HTTPException(status.HTTP_404_NOT_FOUND,
                             detail=f"Postcode '{postcode}' not found")
 
-    response.status_code = 200
+    r[cache_key] = json.dumps(out)
     return out
 
 @router.get("/lga/{lga}")
 def test_lga(lga: str, response: Response):
     """Return testing data for an lga"""
+    response.status_code = 200
+    cache_key = f"tests_{lga}"
+
+    if cache_key in r:
+        return json.loads(r[cache_key])
+
     info = processing.Tests()
     out = info.lga(lga)
 
@@ -29,12 +46,18 @@ def test_lga(lga: str, response: Response):
         raise HTTPException(status.HTTP_404_NOT_FOUND,
                             detail=f"lga '{lga}' not found")
 
-    response.status_code = 200
+    r[cache_key] = json.dumps(out)
     return out
 
 @router.get("/lhd/{lhd}")
 def tests_lhd(lhd: str, response: Response):
     """Return testing data for an lhd"""
+    response.status_code = 200
+    cache_key = f"tests_{lhd}"
+
+    if cache_key in r:
+        return json.loads(r[cache_key])
+
     info = processing.Tests()
     out = info.lhd(lhd)
 
@@ -42,12 +65,18 @@ def tests_lhd(lhd: str, response: Response):
         raise HTTPException(status.HTTP_404_NOT_FOUND,
                             detail=f"lhd '{lhd}' not found")
 
-    response.status_code = 200
+    r[cache_key] = json.dumps(out)
     return out
 
 @router.get("/{location_type}")
 def return_locations(location_type: str, response: Response):
     """Return the list of available postcodes, lgas, and lhd the user can request data for"""
+    response.status_code = 200
+    cache_key = f"tests_{location_type}"
+
+    if cache_key in r:
+        return json.loads(r[cache_key])
+
     info = processing.CaseLocation()
     out =  info.list_locations(location_type)
 
@@ -55,5 +84,5 @@ def return_locations(location_type: str, response: Response):
         raise HTTPException(status.HTTP_404_NOT_FOUND,
                             detail="Invalid location type. Use one of 'lga, postcode, or lhd'")
 
-    response.status_code = 200
+    r[cache_key] = json.loads(out)
     return out
