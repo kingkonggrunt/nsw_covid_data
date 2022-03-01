12fbf307c04180f71599f02213ea71da8806dcf5
kingkonggrunt <little-kk@hotmail.com>
2022-03-01 20:07:18 +1100

    feat: updater clears cache on update

--- updater.py
+++ updater.py
@@ -1,11 +1,20 @@
-from src import data
 import time
+import redis
+from src import data
+from src.cache.redis import RedisDictionary
 
 covid = data.CovidData()
+r = RedisDictionary(redis.Redis, db=5)
 
 def main():
+    """Update the sources every three hours"""
+
     while True:
         print(time.localtime())
+        print("Clearing Cache")
+        for key in r.find_keys("*"):
+            del r[key]
+        print("Cache Cleared")
         covid.update()
         time.sleep(3*60*60)
 
